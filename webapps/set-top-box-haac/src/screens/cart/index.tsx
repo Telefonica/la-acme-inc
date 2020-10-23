30ee9ce999c712bb1a9f5e6f0973bf081af2d3aa
penchochris <garabchris@gmail.com>
2020-10-23 13:47:37 +0200

    css fix

--- webapps/set-top-box-haac/src/screens/cart/index.tsx
+++ webapps/set-top-box-haac/src/screens/cart/index.tsx
@@ -1,11 +1,11 @@
 import './game.scss';
 
 import React, { useEffect } from 'react';
-import { screenReady, NavigableButton, useBackground, useAura, Preloadable } from '@telefonica/la-web-sdk';
+import { screenReady } from '@telefonica/la-web-sdk';
 import Metacritic from '../../components/Metacritic';
 import { Entity, Intent, CartScreenData } from '../../../../../dialogs/src/models';
 
-const CartScreen: React.FC<Preloadable> = ({ screenData }: { screenData: CartScreenData }) => {
+const CartScreen: React.FC<CartScreenData> = (cart: CartScreenData) => {
     return <div></div>;
 };
 
